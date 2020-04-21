3d9cbb4873dbc5b60f80ca2a824375d75f17c431
benakehurst <benakehurst@hotmail.com>
2020-04-21 17:47:20 +0100

    feat(storybook): button component built

--- storybook/src/Components/Atoms/Button/Button.js
+++ storybook/src/Components/Atoms/Button/Button.js
@@ -3,10 +3,37 @@ import classes from './Button.module.css';
 
 import Aux from '../../../hoc/Aux';
 
+/**
+ * Props:
+ * size - 'small' | 'medium' | 'large'
+ * disabled - true | false
+ * corners - 'rounded'
+ * shadow - 'shadow'
+ * buttonType - 'success' | 'error' | 'basic' | 'black' | 'white'
+ * clicked - If true, gets an event to fire when the button is clicked
+ */
+
 const button = (props) => {
   return (
     <Aux>
-      <div className={classes.Button}>Button</div>
+      <button
+        onClick={props.clicked ? (e) => props.clicked(e) : null}
+        disabled={props.disabled}
+        className={[
+          classes.Button,
+          classes[props.size],
+          classes[props.corners],
+          classes[props.shadow],
+          classes[props.buttonType],
+        ].join(' ')}
+      >
+        {/* <div className={classes.TextWrapper}>{props.children}</div> */}
+        <div>
+          {props.children.length > 15
+            ? props.children.substring(0, 15 - 3) + '...'
+            : props.children}
+        </div>
+      </button>
     </Aux>
   );
 };
