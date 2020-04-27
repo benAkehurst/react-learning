a2af81f9f56f161d490fa40d38235fccf0d44794
benakehurst <benakehurst@hotmail.com>
2020-04-27 14:45:07 +0100

    chore(storybook): update to App.js when making the app

--- storybook/src/App.js
+++ storybook/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi there 😀</h1>
+      <p>If you're seeing this, then you ran the app.</p>
+      <p>But this is a Storybook/Design System project...</p>
+      <p>
+        So go back to your terminal, and run <code>npm run storybook</code> 😉
+      </p>
     </div>
   );
 }
