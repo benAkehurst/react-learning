ae0fd51d02263caf4173b9dab4e7ae3bea91cd8c
benakehurst <benakehurst@hotmail.com>
2020-04-27 15:49:22 +0100

    feat(storybook): update image stories and add size prop

--- storybook/src/Components/Atoms/Image/Image.js
+++ storybook/src/Components/Atoms/Image/Image.js
@@ -8,13 +8,18 @@ import Aux from '../../../hoc/Aux';
  * imageLink - a url for the image
  * altText - alt text for the image
  * corners - 'rounded' | 'circle'
+ * size - 'small' | 'medium' | 'large'
  */
 
 const image = (props) => {
   return (
     <Aux>
       <img
-        className={[classes.Image, classes[props.corners]].join(' ')}
+        className={[
+          classes.Image,
+          classes[props.corners],
+          classes[props.size],
+        ].join(' ')}
         src={props.imageLink}
         alt={props.altText}
       />
--- storybook/src/Components/Atoms/Image/Image.stories.js
+++ storybook/src/Components/Atoms/Image/Image.stories.js
@@ -2,31 +2,20 @@ import React from 'react';
 import Image from './Image';
 
 export default {
+  component: Image,
   title: 'Atoms / Image',
 };
 
-export const Baisc = () => (
-  <Image
-    imageLink={'https://via.placeholder.com/150'}
-    altText={'placeholder text'}
-  />
-);
+const imageData = {
+  imageLink: 'https://via.placeholder.com/150',
+  altText: 'placeholder alt text',
+};
 
-export const Rounded = () => (
-  <Image
-    imageLink={'https://via.placeholder.com/150'}
-    altText={'placeholder text'}
-    corners={'rounded'}
-  />
-);
+export const Baisc = () => <Image {...imageData} />;
 
-export const Circle = () => (
-  <Image
-    imageLink={'https://via.placeholder.com/150'}
-    altText={'placeholder text'}
-    corners={'circle'}
-  />
-);
+export const Rounded = () => <Image {...imageData} corners={'rounded'} />;
+
+export const Circle = () => <Image {...imageData} corners={'circle'} />;
 
 export const LargeImage = () => (
   <Image
@@ -34,5 +23,6 @@ export const LargeImage = () => (
       'https://images.unsplash.com/photo-1587270804625-48c99a3cc707?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
     }
     altText={'unsplash image'}
+    size={'large'}
   />
 );
