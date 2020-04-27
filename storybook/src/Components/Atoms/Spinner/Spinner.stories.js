760f41d5d71c32b1f0091082d62596957f364464
benakehurst <benakehurst@hotmail.com>
2020-04-27 16:46:21 +0100

    feat(storybook): update to spinner component

--- storybook/src/Components/Atoms/Spinner/Spinner.stories.js
+++ storybook/src/Components/Atoms/Spinner/Spinner.stories.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Spinner from './Spinner';
 
 export default {
+  component: Spinner,
   title: 'Atoms / Spinner',
+};
+
+const spinnerDefaults = {
+  size: 'large',
 };
 
 export const Small = () => <Spinner size={'small'} color={'basic'} />;
 export const Medium = () => <Spinner size={'medium'} color={'basic'} />;
-export const Large = () => <Spinner size={'large'} color={'basic'} />;
-export const Success = () => <Spinner size={'large'} color={'success'} />;
-export const Error = () => <Spinner size={'large'} color={'error'} />;
-export const Black = () => <Spinner size={'large'} color={'black'} />;
+export const Large = () => <Spinner {...spinnerDefaults} color={'basic'} />;
+export const Success = () => <Spinner {...spinnerDefaults} color={'success'} />;
+export const Error = () => <Spinner {...spinnerDefaults} color={'error'} />;
+export const Black = () => <Spinner {...spinnerDefaults} color={'black'} />;
