37389dc0c0e4538a658bae0f067f933a41b76677
benakehurst <benakehurst@hotmail.com>
2020-04-27 15:49:01 +0100

    feat(storybook): update label with size prop

--- storybook/src/Components/Atoms/Label/Label.js
+++ storybook/src/Components/Atoms/Label/Label.js
@@ -6,12 +6,16 @@ import Aux from '../../../hoc/Aux';
 /**
  * Props:
  * for - needs to be the name of an input the label needs to have
+ * size - 'small' | 'medium' | 'large'
  */
 
 const label = (props) => {
   return (
     <Aux>
-      <label className={classes.Label} htmlFor={props.for}>
+      <label
+        className={[classes.Label, classes[props.size]].join(' ')}
+        htmlFor={props.for}
+      >
         {props.children}
       </label>
     </Aux>
