e27c7dad11ccc5d1d049fed9e36821a72a7c5abc
benakehurst <benakehurst@hotmail.com>
2020-04-27 16:45:55 +0100

    feat(storybook): label component updates

--- storybook/src/Components/Atoms/Label/Label.stories.js
+++ storybook/src/Components/Atoms/Label/Label.stories.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Label from './Label';
 
 export default {
+  component: Label,
   title: 'Atoms / Label',
 };
 
 export const Baisc = () => <Label>Basic Label</Label>;
+export const Small = () => <Label size={'small'}>Small Label</Label>;
+export const Medium = () => <Label size={'medium'}>Medium Label</Label>;
+export const Large = () => <Label size={'large'}>Large Label</Label>;
--- storybook/src/Components/Atoms/Label/Label.test.js
+++ storybook/src/Components/Atoms/Label/Label.test.js
@@ -9,14 +9,22 @@ describe('<Label />', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Label for={'test'}>Basic Label</Label>);
+    wrapper = shallow(
+      <Label for={'test'} size={'small'}>
+        Basic Label
+      </Label>
+    );
   });
 
   it('should render the compoent', () => {
     expect(wrapper).toBeTruthy();
   });
 
-  it('should have a for prop', () => {
+  it('should have a htmlFor prop', () => {
     expect(wrapper.props().children.props.htmlFor).toBeTruthy();
+  });
+
+  it('should have a size prop', () => {
+    expect(wrapper.props().children.props.className).toEqual('Label small');
   });
 });
